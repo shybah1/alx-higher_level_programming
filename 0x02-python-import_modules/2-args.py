7e181b635cb35ac6c7641daf485c6e128834c864
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-08-25 05:24:35 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     i = len(sys.argv) - 1
 
     if i == 0:
@@ -9,7 +8,7 @@ if __name__ == "__main__":
     elif i == 1:
         print("{} argument:".format(i))
     else:
-        print("{} argument:".format(i))
+        print("{} arguments:".format(i))
 
     if i >= 1:
         i = 0
