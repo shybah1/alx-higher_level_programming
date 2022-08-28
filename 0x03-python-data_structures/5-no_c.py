46dca76015977fa9f130a1b782fe8889add86817
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-08-28 12:21:34 -0700

    no_c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    length = len(my_string)
+
+    j = 0
+
+    new_string = my_string[:]
+
+    for i in range(length):
+        if (my_string[i] == 'c' or my_string[i] == 'C'):
+            new_string = my_string[:(i - j)] + my_string[(i + 1):]
+
+            j += 1
+
+    return (new_list)
