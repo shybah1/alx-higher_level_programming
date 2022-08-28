cdf66e85bc2e54dd1158fea28f98160672f5584b
Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-08-28 07:25:33 -0700

    fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-
-    if not my_list:
-        pass
-    else:
-        my_list.reverse()
-        for i in range(my_list):
-            print("{}".format(my_list[i]))
