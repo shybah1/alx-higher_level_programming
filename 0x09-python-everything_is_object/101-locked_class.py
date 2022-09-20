35fc1cc9d78ca4c729cefc3c80419a4fe913d057
Abdulsalam Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-09-20 21:28:05 +0100

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,6 +8,10 @@ dynmaically created attributes
 
 
 class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
     __slots__ = ['first_name']
 
     def __init__(self):
