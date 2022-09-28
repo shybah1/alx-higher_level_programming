ffc118106238d537270076fc1f33215c22ced38a
Abdulsalam Abdulmutalib <abdulsalam.abdulmutalib1@gmail.com>
2022-09-28 20:36:03 +0100

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-# 1-my_list.py
-"""Defines an inherited list class MyList."""
+"""
+contains the MyList class
+"""
 
 
 class MyList(list):
-    """Implements sorted printing for the built-in list class."""
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
 
     def print_sorted(self):
-        """Print a list in sorted ascending order."""
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)
+        """prints the sorted list"""
+        print(sorted(self))
